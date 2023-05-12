5b1b82c8d3d5b6a2013933e74d19d2e352375bb9
Iris Ortega <irisorca13@gmail.com>
2023-05-12 11:25:55 +0200

    Test

--- Iris_Ortega-Ferran_Rodriguez.py
+++ Iris_Ortega-Ferran_Rodriguez.py
@@ -1,1 +1,15 @@
-# Hola
+import numpy as np 
+import math as m
+import argparse
+import pandas as pd
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog = 'Iris_Ortega-Ferran_Rodriguez.py', description = 'Computes')
+    parser.add_argument('inpath', help = 'Path of the input file. Example: /home/iris/Desktop/UNI/8è/MQ_N-cossos/Entrega/MQNCSU_Gross-Pitaevskii/')
+    arguments = parser.parse_args()
+
+# Directoris
+    inpath = arguments.inpath
+    data = pd.read_csv(inpath, sep=" ", header=None)
+    data.columns = ["a0", "n1", "step", "aa", "time", "alpha", "iter"]
+    print(data)
